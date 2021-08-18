c1d242521881dbe39663423123cd8f432af85932
abrasov <abrasovs@yandex.ru>
2021-08-18 11:47:09 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.min.apply(this, array);
+    }
+    return 0;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (Array.isArray(array) && array.length) {
+        return Math.max.apply(this, array);
+    }
+    return 0;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (Array.isArray(array) && array.length) {
+        return array.reduce((a, b) => a + b) / array.length;
+    }
+    return 0;
+};
